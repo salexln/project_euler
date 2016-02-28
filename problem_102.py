0c5ce7dc2cdbce76968b9c758384e14e14fe000d
alex <salexln@gmail.com>
2016-02-28 23:48:42 +0200

    102

--- problem_102.py
+++ problem_102.py
@@ -1,21 +1,18 @@
 """
+Referece: https://en.wikipedia.org/wiki/Barycentric_coordinate_system
 
 Suppose we have a triangle: v1, v2, v3  (vi = (xi, yi) )
 
 Each point P inside the triangle can we written:
 p = a*v1 + b*v2 + c*v3, a+b+c = 1, a,b,c >= 0
 
-We'll find a, b, c:
+We'll find a, b, c by solving the following equation:
 
-(a)
-   = T^-1 * (x - x3)
-(b)
 
-c = 1 - a - b
+x1 x2 x3   a    0
+y1 y2 y3 * b =  0
+ 1  1  1   c    1
 
-    (x1 - x3, x2 - x3)
-T =  
-	(y1 - y3, y2 - y3)
 
 """
 
@@ -32,31 +29,13 @@ def check_barycentric_coordinate(v1, v2, v3):
 
 	sol = np.array([0, 0, 1])
 
-	# print T 
-	# print sol
-
 	a,b,c =  np.linalg.solve(T, sol)
 	
-	# print a,b,c
 	if a > 0 and b > 0 and c > 0:				
-		# if a+b+c - 1.0 == 0:
 		return True	
 	return False
 
 
-# a = (-340,495)
-# b = (-153,-910)
-# c = (835,-947)
-
-# print check_barycentric_coordinate(a,b,c)
-
-
-
-# x = (-175,41)
-# y = (-421,-714)
-# z = (574,-645)
-
-# print check_barycentric_coordinate(x,y,z)
 
 
 count  = 0
@@ -68,7 +47,6 @@ with open('data/p102_triangles.txt', 'r') as f:
 		y = (int(line.split(',')[2]),  int(line.split(',')[3]))
 		z = (int(line.split(',')[4]),  int(line.split(',')[5]))
 
-		# print x,y,z
 		if check_barycentric_coordinate(x,y,z):
 			count += 1
 print 'count = ', count
